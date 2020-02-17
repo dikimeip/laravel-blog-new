ef5f8b6852dc16a8b49c4fa1e69660b523257f5c
dikimeip <pm159145@gmail.com>
2020-02-17 14:20:57 +0700

    melakukan instalasi template user

--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,16 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/admin','LoginController@index');
 Route::get('/wp-admin/logout','AdminController@logout')->name('logout');
 
+// user
+Route::get('/','UserController@index')->name('user.index');
+
+
+
+// admin
 Route::post('/admin','LoginController@do_Login')->name('login');
 Route::get('/wp-admin/index','AdminController@index')->name('AdminPage');
 Route::get('/wp-admin/artikel','ArtikelController@index')->name('artikelPage');
@@ -26,7 +29,6 @@ Route::get('/wp-admin/artikel/update/{id}','ArtikelController@update')->name('ar
 Route::get('/wp-admin/artikel/destroy/{id}','ArtikelController@destroy')->name('artikelDestroy');
 Route::post('/wp-admin/artikel/store/{id}','ArtikelController@changeArtikel')->name('artikelRubah');
 Route::post('/wp-admin/artikel/cari','ArtikelController@cari')->name('artikelCari');
-
 Route::get('/wp-admin/artikel/add','ArtikelController@add')->name('addArtikel');
 Route::post('/wp-admin/artikel/add','ArtikelController@store')->name('addArtikel');
 Route::get('/wp-admin/kategori','KategoriController@index')->name('kategoriPage');
