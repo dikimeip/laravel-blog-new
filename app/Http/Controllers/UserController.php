e6c182f9daf4166947671d583b2ad377a643190d
dikimeip <pm159145@gmail.com>
2020-02-17 14:41:53 +0700

    menemapilkan data terbaru di halaman user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\artikel;
+use App\kategori;
 
 class UserController extends Controller
 {
     public function index()
     {
-    	return view('user.dasboard') ;
+    	$kategori = kategori::all();
+    	$limitnew = artikel::latest()->get()->random(2);
+    	return view('user.dasboard',compact('kategori','limitnew')) ;
     }
 }
--- resources/views/user/dasboard.blade.php
+++ resources/views/user/dasboard.blade.php
@@ -45,12 +45,10 @@
 
 						<!-- nav -->
 						<ul class="nav-menu nav navbar-nav">
-							<li><a href="category.html">News</a></li>
-							<li><a href="category.html">Popular</a></li>
-							<li class="cat-1"><a href="category.html">Web Design</a></li>
-							<li class="cat-2"><a href="category.html">JavaScript</a></li>
-							<li class="cat-3"><a href="category.html">Css</a></li>
-							<li class="cat-4"><a href="category.html">Jquery</a></li>
+							<li><a href="">News</a></li>
+							@foreach($kategori as $k)
+							<li class="cat-1"><a href="">{{$k->nama_kategori}}</a></li>
+							@endforeach
 						</ul>
 						<!-- /nav -->
 
@@ -137,33 +135,24 @@
 				<!-- row -->
 				<div class="row">	
 					<!-- post -->
+					@foreach($limitnew as $ln)
 					<div class="col-md-6">
 						<div class="post post-thumb">
-							<a class="post-img" href="blog-post.html"><img src="./img/post-1.jpg" alt=""></a>
+							<a class="post-img" href="blog-post.html"><img style="width: 560px;height: 350px" src="{{asset('image/'.$ln->gambar)}}" alt=""></a>
 							<div class="post-body">
 								<div class="post-meta">
-									<a class="post-category cat-2" href="category.html">JavaScript</a>
-									<span class="post-date">March 27, 2018</span>
+									<a class="post-category cat-2" href="category.html">{{$ln->kategori->nama_lategori}}</a>
+									<span class="post-date">{{$ln->tanggal}}</span>
 								</div>
-								<h3 class="post-title"><a href="blog-post.html">Chrome Extension Protects Against JavaScript-Based CPU Side-Channel Attacks</a></h3>
+								<h3 class="post-title"><a href="blog-post.html">{{$ln->judul}}</a></h3>
 							</div>
 						</div>
 					</div>
+					@endforeach
 					<!-- /post -->
 
 					<!-- post -->
-					<div class="col-md-6">
-						<div class="post post-thumb">
-							<a class="post-img" href="blog-post.html"><img src="./img/post-2.jpg" alt=""></a>
-							<div class="post-body">
-								<div class="post-meta">
-									<a class="post-category cat-3" href="category.html">Jquery</a>
-									<span class="post-date">March 27, 2018</span>
-								</div>
-								<h3 class="post-title"><a href="blog-post.html">Ask HN: Does Anybody Still Use JQuery?</a></h3>
-							</div>
-						</div>
-					</div>
+					
 					<!-- /post -->
 				</div>
 				<!-- /row -->
